0c8200a6af879e336a2a21d312da85e00697bc52
TinTDang <tin2dang@gmail.com>
2018-08-07 19:43:34 -0500

    added moment, firebase, and jquery CDNs

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB1BU3CEKnkamzMpUF7sy_8XoQaZ3Y4MFw",
+    authDomain: "train-schedule-ef602.firebaseapp.com",
+    databaseURL: "https://train-schedule-ef602.firebaseio.com",
+    projectId: "train-schedule-ef602",
+    storageBucket: "",
+    messagingSenderId: "1092064675854"
+};
+firebase.initializeApp(config);
